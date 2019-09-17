05471d6574ca6521bbe00ddd4c5d62636f29058b
Ernesto Costa <ernestogcosta@gmail.com>
2019-09-17 13:43:24 -0300

    Terminando a separação dos grupos e identificação dos pai e mãe, agora é preciso fazer o cruzamento

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, random
 from math import ceil
 
 TAMANHO_MAX = int(80)
@@ -15,6 +15,7 @@ def main(): #{
         print(f'{newPop[i][0:5]}\ttamanho: {newPop[i][5]}\tvalor: {newPop[i][6]}')
 
     separaGrupos(newPop, tabela)
+
 #}
 
 def gerarPop(numInd, tabela):#{
@@ -54,23 +55,70 @@ def verificarFitness(individuo, tabela): #{
 
 def separaGrupos(populacao, tabela): #{
     individuos = populacao
-    individuos.sort(key=keyValor, reverse=True)
+    individuos.sort(key=keyValor1, reverse=True)
+    meio = ceil(len(populacao)/2)
+
+    chancePai = random()*100
+    chancePai = random()*100
+    totalPai = 0
+    totalMae = 0
+    pai = []
+    mae = []
+    probPais = []
+    probMaes = []
+    probInd = []
+
+    for i in range(0, len(individuos)):
+        if (i < meio):
+            totalPai += individuos[i][6]
+        else:
+            totalMae += individuos[i][6]
+
+    for i in range(0, len(individuos)):
+        prob = 0
+        probInd = []
+        if (i < meio):
+            prob = individuos[i][6] * 100 / totalPai
+            probInd.append(prob)
+            probInd.append(i)
+            probPais.append(probInd)
+        else:
+            prob = individuos[i][6] * 100 / totalMae
+            probInd.append(prob)
+            probInd.append(i)
+            probMaes.append(probInd)
+    probPais.sort(key=keyValor2, reverse=True)
+    probMaes.sort(key=keyValor2, reverse=True)
 
     ##########################################################################################
     ## SÓ PRA TESTAR, APAGAR ISSO DEPOIS #####################################################
     ##########################################################################################
-    print('pop sorted')
-    for i in range(0,5):
+    # duas últimas colunas são tamanho e por último valor
+    print('ordensado:')
+    for i in range(0, (len(individuos))):
         print(f'{individuos[i][0:5]}\ttamanho: {individuos[i][5]}\tvalor: {individuos[i][6]}')
+    print('probPais:')
+    for i in range(0, len(probPais)):
+        print(probPais[i])
+    print('probMaes:')
+    for i in range(0, len(probMaes)):
+        print(probMaes[i])
     ##########################################################################################
+    # pai = individuos[probPais[0][1]]
+    # mae = individuos[probMaes[0][1]]
 
+    # Agora dá pra saber quem são o pai e a mãe, tem que fazer o cruzamento
 
 
 #}
 
-def keyValor(objeto):
+def keyValor1(objeto):
     #isso apenas precisa para fazer o sort, pois pega qual parte do objeto vai ser baseado o sort
     return objeto[6]
+
+def keyValor2(objeto):
+    #isso apenas precisa para fazer o sort, pois pega qual parte do objeto vai ser baseado o sort
+    return objeto[0]
 
 if(__name__ == "__main__"): #{
     main()
